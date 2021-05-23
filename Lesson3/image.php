f0445d00fe053933c4f7883f6c4fb39cc61ed92f
art-cherepan <60571825+art-cherepan@users.noreply.github.com>
2021-05-23 18:47:29 +0900

    Delete Lesson3 directory

--- Lesson3/image.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $arr_images = [1 => 'dog1.jpg', 2 => 'dog2.jpg', 3 => 'dog3.jpg', 4 => 'dog4.jpg'];
-    if (isset($_GET['id'])) {
-?>
-<img src="media/<?php echo $arr_images[$_GET['id']]; ?>">
-<?php } ?>
