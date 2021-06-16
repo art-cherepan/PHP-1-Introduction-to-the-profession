9453bacc6a16afc28e94e6f5ddbc50e5548ebb57
art-cherepan <60571825+art-cherepan@users.noreply.github.com>
2021-06-16 15:35:55 +0900

    Delete Lesson6 directory

--- Lesson6/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/classes/GuestBook.php';
-
-$guestBook = new GuestBook(__DIR__ . '/guestBook.txt');
-$guestBook->append('Первый коммент');
-$guestBook->append('Второй коммент');
-$guestBook->append('Третий коммент');
-$guestBook->save();
