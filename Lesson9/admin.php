5df2f5c262af9905fb88b58889726b506d6ed776
art-cherepan <60571825+art-cherepan@users.noreply.github.com>
2021-08-09 16:52:58 +0900

    Delete Lesson9 directory

--- Lesson9/admin.php
+++
@@ -1,9 +0,0 @@
-<form action="/Lesson9/addProduct.php" method="post"
-      enctype="multipart/form-data">
-    <p>Введите наименование товара: <input type="text" name="name"></p>
-    <p>Введите описание товара: <textarea name="description"></textarea></p>
-    <p>Укажите цену товара: <input type="number" min="1" step="any" name="price"/></p>
-    <input type="file" name="img">
-    <button type="submit">Отправить файл</button>
-</form>
-
--- Lesson9/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/classes/DB.php';
-require_once __DIR__ . '/classes/Models/Product.php';
-
-$product = new Product('test', 'test', 'test', 1);
-$product->setId(1);
-$product->getReviews();
-var_dump($product);
